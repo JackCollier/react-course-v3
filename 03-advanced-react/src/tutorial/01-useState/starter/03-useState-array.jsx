7c95d94359484ee356b6597bbab56c7621d07d6e
JacvaSipt <steamcow12@hotmail.com>
2023-05-21 15:25:27 +0100

    create dynamic array with useState

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    if (id.id === people.id) {
+      setPeople(people.filter((x) => x.id !== id));
+    }
+  };
+  const clearAll = () => {
+    setPeople((people.length = 0));
+  };
+  return (
+    <div>
+      {people.map((ppl) => {
+        const { id, name } = ppl;
+        return (
+          <div>
+            <h4>{name}</h4>
+            <button onClick={() => removePerson(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button onClick={clearAll}>Clear</button>
+    </div>
+  );
 };
 
 export default UseStateArray;
